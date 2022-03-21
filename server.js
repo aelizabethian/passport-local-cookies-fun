89273429900d8304a656c5ee9e6083700ce22110
aelizabethian <amanda.richardson2@gmail.com>
2022-03-21 12:34:38 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const passport = require("passport");
+const bodyParser = require("body-parser");
+
+const LocalStrategy = require("passport-local").Strategy;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  console.log(user);
+  done(null, user);
+});
+
+passport.use(
+  "login",
+  new LocalStrategy((username, password, done) => {
+    const authenticated = username === "John" && password === "Smith";
+
+    if (authenticated) {
+      return done(null, { myUser: "user", myID: 1234 });
+    } else {
+      return done(null, false);
+    }
+  })
+);
+
+app.post(
+  "/login",
+  passport.authenticate("login", {
+    successRedirect: "/success",
+    failureRedirect: "/login",
+
+  })
+);
+
+app.get("/success", (req, res) => {
+  res.send("Hey, hello from the server!");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/login.html");
+});
+
+app.listen(8000);
